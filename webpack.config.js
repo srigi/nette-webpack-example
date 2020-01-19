ea9c7ab6265e2a14233b0c1979629e383ccfd5e3
srigi <srigi@srigi.sk>
2020-01-19 20:58:25 +0100

    Add Webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+
+module.exports = {
+  entry: './www/js/app.js',
+  mode: (process.env.NODE_ENV === 'production') ? 'production' : 'development',
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'www', 'assets'),
+  },
+};
