063fc69b6f94bd2bc9689edc1944e077c1fcfb1b
srigi <srigi@srigi.sk>
2020-01-19 20:59:51 +0100

    Manage frontend libs via Webpack

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,8 @@
+import naja from 'naja';
+import netteForms from 'nette-forms';
+
+
+window.Nette = netteForms;
+
 document.addEventListener('DOMContentLoaded', naja.initialize.bind(naja));
+netteForms.initOnLoad();
